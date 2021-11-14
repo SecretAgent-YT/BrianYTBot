e2663fb0312887e14a40d1bb73d750d889878ca4
SecretAgent <75268452+SecretAgent-YT@users.noreply.github.com>
2021-11-13 18:58:53 -0800

    Add files via upload

---
+++ src/main/java/me/secretagent/brianytbot/BrianYT.java
@@ -0,0 +1,18 @@
+package me.secretagent.brianytbot;
+
+public class BrianYT {
+
+    public static void main(String args[]) {
+        if (args.length < 1) {
+            System.out.println("Did not specify token!");
+            System.exit(-1);
+        }
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+    }
+
+}

37af1565232875c8a1f5660382a1b397708fa59a
SecretAgent <75268452+SecretAgent-YT@users.noreply.github.com>
2021-11-13 19:10:13 -0800

    Add files via upload

--- src/main/java/me/secretagent/discord/Start.java
+++ src/main/java/me/secretagent/discord/Start.java
@@ -1,8 +1,14 @@
 package me.secretagent.discord;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,6 +16,18 @@ public class Start {
 
     public static void main(String args[]) throws Exception {
         JSONObject object = new JSONObject();
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange httpExchange) throws IOException {
+                byte[] response = "B_RIANYT BOT".getBytes();
+                httpExchange.sendResponseHeaders(200, response.length);
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(response);
+                os.close();
+            }
+        });
+        server.start();
         object.put("token", "OTAzMDI5NTk0NzAwMDE3NjY0.YXnB2A.Gh6VvYck7jY3fWX2htocq9r4ezc");
         object.put("prefix", ";");
         object.put("uploadChannelID", "762822345639198742");
